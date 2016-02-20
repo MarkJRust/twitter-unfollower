575c5dff16d8aa541050217ac9fb1566911e8b61
MarkJRust <markjrust@gmail.com>
2016-02-20 05:57:20 -0500

    Place followers in a list with checkboxes

--- unfollow.py
+++ unfollow.py
@@ -1,22 +1,41 @@
 __author__ = 'MarkJRust'
 import tweepy
+from Tkinter import *
+import tkMessageBox
+import Tkinter
 
-consumer_key = ""
+
+consumer_key = 	""
 consumer_secret = ""
 access_token = ""
 access_token_secret = ""
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
+
 def getAndPrintFollowers(twitterHandle):
     users = tweepy.Cursor(api.followers, screen_name=twitterHandle).items()
     for user in users:
         print(user.screen_name)
+        CBs = Checkbutton(topWindow, text=user.screen_name)
+        CBs.pack()
 
-getAndPrintFollowers("MarkJRust")
+
+#def getSelectedCheckboxes():
+
+
+topWindow = Tkinter.Tk()
+Lb1= Listbox(topWindow)
+
+getAndPrintFollowers("MHacks7Test")
+#getSelectedCheckboxes()
+
+Lb1.pack()
+topWindow.mainloop()
 print("Done fetching and printing followers")
 
 
