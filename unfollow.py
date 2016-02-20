3a4dae50db8f6370850ca4309f7d7dcd17b117ea
MarkJRust <markjrust@gmail.com>
2016-02-20 08:58:38 -0500

    Using easygui Simple implementation for testing

--- unfollow.py
+++ unfollow.py
@@ -1,42 +1,42 @@
 __author__ = 'MarkJRust'
 import tweepy
-from Tkinter import *
-import tkMessageBox
-import Tkinter
-
+import easygui as eg
 
 consumer_key = 	""
 consumer_secret = ""
 access_token = ""
 access_token_secret = ""
 
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-
 def getAndPrintFollowers(twitterHandle):
     users = tweepy.Cursor(api.followers, screen_name=twitterHandle).items()
+    userList =[];
     for user in users:
-        print(user.screen_name)
-        CBs = Checkbutton(topWindow, text=user.screen_name)
-        CBs.pack()
+        userList.append(user.screen_name)
+    question = "Who do you not want following you anymore? We will block and unblock them to remove the connection"
+    title = "Potential Unfollower"
+    listOfOptions = userList
+    choice = eg.multchoicebox(question , title, listOfOptions)
+    return choice
 
+def block(unfollowers):
+    for user in unfollowers:
+        api.create_block(user)
 
-#def getSelectedCheckboxes():
+def unblock(unfollowers):
+    for user in unfollowers:
+        api.destroy_block(user)
 
-
-topWindow = Tkinter.Tk()
-Lb1= Listbox(topWindow)
-
-getAndPrintFollowers("MHacks7Test")
-#getSelectedCheckboxes()
-
-Lb1.pack()
-topWindow.mainloop()
-print("Done fetching and printing followers")
+unfollowers = getAndPrintFollowers("MHacks7Test")
+eg.msgbox("Done fetching and printing followers")
+block(unfollowers)
+eg.msgbox("Done blocking users")
+unblock(unfollowers)
+eg.msgbox("Done unblocking users")
 
 
 
